e407d17001ad555387a745e1a03fbf554e8096ee
Stephen Bannasch <stephen.bannasch@gmail.com>
2019-05-19 18:32:37 -0400

    access images managed by Parcel in Elm
    Use parcel require to resolve pathnames for png, jpg, jpeg, svg
    and gif image types stored in ./assets/images
    
    Create an object with the image file names converted to camelCase names
    keys and Parcel-generated paths as values.
    
    Return object to Elm on startup through flags.

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,126 @@
-module Main exposing (main)
+module Main exposing (Model, Msg(..), init, main, update, view)
 
-import Html
+import Browser
+import Browser.Navigation
+import Html exposing (Html, div, h1, img, p, text)
+import Html.Attributes exposing (class, src)
+import Url exposing (Url)
 
 
+
+---- PROGRAM ----
+
+
+type alias Images =
+    { actionOkIcon : String
+    , actionCancelIcon : String
+    , appBabelfishIcon : String
+    , wetPinkTulipSmall : String
+    , fivePointedStar : String
+    }
+
+
+main : Program Images Model Msg
 main =
-    Html.text "Hello rainy day in Boston."
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlRequest = ClickedLink
+        , onUrlChange = ChangedUrl
+        }
+
+
+init : Images -> Url -> Browser.Navigation.Key -> ( Model, Cmd Msg )
+init images url key =
+    ( initialModel images url key, Cmd.none )
+
+
+
+---- MODEL ----
+
+
+type alias Model =
+    { content : String
+    , url : Url.Url
+    , key : Browser.Navigation.Key
+    , img : Images
+    }
+
+
+initialModel : Images -> Url.Url -> Browser.Navigation.Key -> Model
+initialModel images url key =
+    { content = "Hello rainy day in Boston"
+    , url = url
+    , key = key
+    , img = images
+    }
+
+
+subscriptions : Model -> Sub msg
+subscriptions model =
+    Sub.none
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = NoOp
+    | ClickedLink Browser.UrlRequest
+    | ChangedUrl Url.Url
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        ClickedLink urlRequest ->
+            ( model
+            , Cmd.none
+            )
+
+        ChangedUrl url ->
+            ( model
+            , Cmd.none
+            )
+
+
+
+---- VIEW ----
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "Elm App"
+    , body =
+        [ div [ class "flex-grid" ]
+            [ div [ class "upper-left" ]
+                [ img [ src model.img.actionOkIcon, class "icon" ] []
+                ]
+            , div
+                [ class "upper-right" ]
+                [ img [ src model.img.actionCancelIcon, class "icon" ] []
+                ]
+            , div [ class "lower-left" ]
+                [ img [ src model.img.appBabelfishIcon, class "icon" ] []
+                ]
+            , div [ class "lower-center" ]
+                [ img [ src model.img.fivePointedStar, class "icon" ] []
+                ]
+            , div [ class "lower-right" ]
+                [ img [ src model.img.wetPinkTulipSmall, class "icon" ] []
+                ]
+            , div [ class "col" ] []
+            , div [ class "col" ]
+                [ h1 [] [ text "Hello Elm App" ]
+                , p [] [ text model.content ]
+                ]
+            , div [ class "col" ] []
+            ]
+        ]
+    }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,39 @@
+import './main.scss'
+
+const removeFileExtension = (str) => str.replace(/\.(png|jpe?g|svg|gif)$/, '');
+
+const toDashes = (str) => str.replace('_', '-');
+
+const toCamelCase = (str) => str.replace(/(?:^\w|[A-Z]|\b\w)/g, (ltr, idx) => idx === 0 ? ltr.toLowerCase() : ltr.toUpperCase()).replace(/(\s|\W)+/g, '');
+
+const toElmName = (str) => toCamelCase(toDashes(removeFileExtension(str)));
+
+const isString = (a) => { return typeof a === "string" };
+
+function convertPaths(imagePaths) {
+  let images = {};
+  let imageTypeMatcher = RegExp("(png|jpg|jpeg|gif|svg)");
+  Object.keys(imagePaths).forEach(function(key) {
+    if (isString(imagePaths[key])) {
+      images[toElmName(key)] = imagePaths[key];
+    } else {
+      let imageType = Object.keys(imagePaths[key])[0];
+      if (imageTypeMatcher.test(imageType)) {
+        images[toElmName(key)] = imagePaths[key][imageType]
+      }
+    }
+  })
+  return images;
+}
+
+// Tell Parcel this JS file uses the images in ../assets/images
+// image names are converted to 'camelCase' and file suffixes are removed
+const imagePaths = require('../assets/images/*.*');
+const images = convertPaths(imagePaths)
+
 const { Elm } = require('./Main.elm');
 
 Elm.Main.init({
-  node: document.querySelector('main')
+  node: document.querySelector('main'),
+  flags: images
 });
